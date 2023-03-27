bfbd2849e7af827e97a52065da2302ad555d3fd3
Raphael Wagner <31208448+raalwa@users.noreply.github.com>
2023-03-27 20:54:45 +0200

    Add midi input to pin output

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,86 @@
-#include <Arduino.h>
+/*
+ * MIDIUSB_test.ino
+ *
+ * Created: 4/6/2015 10:47:08 AM
+ * Author: gurbrinder grewal
+ * Modified by Arduino LLC (2015)
+ */
 
-void setup() {
-  // put your setup code here, to run once:
+#include "MIDIUSB.h"
+
+#define MOSFET_PIN 7
+
+#define DRUM_NOTE 0x24
+
+// First parameter is the event type (0x09 = note on, 0x08 = note off).
+// Second parameter is note-on/note-off, combined with the channel.
+// Channel can be anything between 0-15. Typically reported to the user as 1-16.
+// Third parameter is the note number (48 = middle C).
+// Fourth parameter is the velocity (64 = normal, 127 = fastest).
+
+void noteOn(byte channel, byte pitch, byte velocity)
+{
+  midiEventPacket_t noteOn = {0x09, 0x90 | channel, pitch, velocity};
+  MidiUSB.sendMIDI(noteOn);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void noteOff(byte channel, byte pitch, byte velocity)
+{
+  midiEventPacket_t noteOff = {0x08, 0x80 | channel, pitch, velocity};
+  MidiUSB.sendMIDI(noteOff);
+}
+
+void setup()
+{
+  Serial.begin(115200);
+
+  pinMode(MOSFET_PIN, OUTPUT);
+}
+
+// First parameter is the event type (0x0B = control change).
+// Second parameter is the event type, combined with the channel.
+// Third parameter is the control number number (0-119).
+// Fourth parameter is the control value (0-127).
+
+void controlChange(byte channel, byte control, byte value)
+{
+  midiEventPacket_t event = {0x0B, 0xB0 | channel, control, value};
+  MidiUSB.sendMIDI(event);
+}
+
+void midiToPin(midiEventPacket_t received){
+  switch(received.byte2){
+    case DRUM_NOTE:
+      Serial.println("Activating MOSFET");
+      if(received.byte3 != 0){
+        digitalWrite(MOSFET_PIN, HIGH);
+      }
+      else{
+        digitalWrite(MOSFET_PIN, LOW);
+      }
+      break;
+  }
+}
+
+void loop()
+{
+  midiEventPacket_t rx;
+  do
+  {
+    rx = MidiUSB.read();
+    if (rx.header != 0)
+    {
+      Serial.print("Received: ");
+      Serial.print(rx.header, HEX);
+      Serial.print("-");
+      Serial.print(rx.byte1, HEX);
+      Serial.print("-");
+      Serial.print(rx.byte2, HEX);
+      Serial.print("-");
+      Serial.println(rx.byte3, HEX);
+
+      // handle tones
+      midiToPin(rx);
+    }
+  } while (rx.header != 0);
 }
